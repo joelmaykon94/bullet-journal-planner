9ce939d6dfce93779977399f9576f0e6ac793ef3
Joel Maykon <joelmaykon94@users.noreply.github.com>
2026-07-19 22:58:03 -0300

    fix: ensure env.js is un-cached and has backward compatibility

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // Lê do env.js gerado dinamicamente via script e exposto em window.env
-  supabaseUrl: (window as any).env?.VITE_SUPABASE_URL || '',
-  supabaseKey: (window as any).env?.VITE_SUPABASE_ANON_KEY || ''
+  supabaseUrl: (window as any).env?.VITE_SUPABASE_URL || (window as any).env?.NG_APP_SUPABASE_URL || '',
+  supabaseKey: (window as any).env?.VITE_SUPABASE_ANON_KEY || (window as any).env?.NG_APP_SUPABASE_ANON_KEY || ''
 };
